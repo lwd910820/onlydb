a5839f55000dc61142a7a54e1b4b5d0a28550c7c
lwd910820 <445638825@qq.com>
2019-05-13 16:26:52 +0800

    12

--- src/main/java/com/onlydb/test.java
+++ src/main/java/com/onlydb/test.java
@@ -6,10 +6,7 @@ import com.sun.org.apache.xerces.internal.impl.dv.xs.HexBinaryDV;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println(Float.intBitsToFloat(Integer.valueOf("4015C28F",16)));
-//        System.out.println(Integer.toHexString(Float.floatToIntBits(0.51f)));
-        String strHex = Integer.toHexString(279);
-        System.out.println(strHex);
+
     }
 
 }
